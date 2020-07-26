3da1f11fcb50d6ac9e1644d9c828652ab9791d61
SHOUKAT GHOUSE <shoukatghouse.as@gmail.com>
2020-07-26 16:52:41 +0530

    Update Worker.py

--- Worker.py
+++ Worker.py
@@ -31,8 +31,8 @@ def partitionedWorker(partId,numPaths):
     csv_buffer = StringIO()
     mydf.to_csv(csv_buffer)
     session = boto3.Session(
-    aws_access_key_id="AKIAWBCUGUJV7HNCSX5S",
-    aws_secret_access_key="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",
+    aws_access_key_id="*****",
+    aws_secret_access_key="******",
     )
     
     s3_resource = session.resource('s3')
@@ -52,33 +52,7 @@ numPaths=int(sys.argv[2])
 print ("Start Time for Partition {} processing = ".format(partId),datetime.datetime.now())
 partitionedWorker(partId,numPaths)
 
-# from io import StringIO
-# import pandas as pd
-# import boto3
-# mydf=pd.DataFrame(columns = ['PartId', 'ID', 'Name','SimulatedVal'])
-# mypart=df.loc[df["PartId"]==partId]
-# #print(mypart)
 
-# for i in range(numPaths):
-    
-#     for index, row in mypart.iterrows():
-#         #print(row['PartId'], row[' ID'],row['Name'],index)
-#         simData=[[row['PartId'],row[' ID'],row['Name'],i]]
-#         simDFOut=pd.DataFrame(simData, columns = ['PartId', 'ID','Name','SimulatedVal']) 
-#         mydf=mydf.append(simDFOut)
-#         #print(mydf.head())
-# bucket = 'sg-test-bbl-eks'  # already created on S3
-# csv_buffer = StringIO()
-# mydf.to_csv(csv_buffer)
-# session = boto3.Session(
-# aws_access_key_id="AKIAWBCUGUJV7HNCSX5S",
-# aws_secret_access_key="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",
-# )
-
-# s3_resource = session.resource('s3')
-# s3_resource.Object(bucket, 'output/mydf.csv').put(Body=csv_buffer.getvalue())
-# #mydf.to_csv("s3://sg-test-bbl-eks/output/{}.csv".format(partId))
-# #return mydf
 
 
 
